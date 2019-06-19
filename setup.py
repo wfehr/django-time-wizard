55346d15bfdac9abf22d2883047e0878fa3c6d6c
Wolfgang Fehr <dev@wfehr.de>
2019-06-19 13:58:15 +0200

    fixed author, modified dependencies

--- setup.py
+++ setup.py
@@ -1,38 +1,39 @@
-import os
-
 from setuptools import find_packages, setup
+from time_wizard import __version__
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-time-wizard',
-    version='0.1.0',
-    packages=find_packages(),
+    version=__version__,
+    packages=find_packages(exclude=['tests']),
     install_requires=[
-        'django-polymorphic',
-        'django<2.0',
-        'holidays',
+        # django-polymorphic is currently not working with django 2.2
+        # https://github.com/django-polymorphic/django-polymorphic/issues/382
+        'django>=1.11,<2.2',
+        'django-polymorphic>=0.7,<2.1',
+        'holidays==0.9.10',
     ],
     include_package_data=True,
     description='Date and time dependend content manipulation',
-    long_description=README,
+    long_description=open('README.md').read(),
     url='https://github.com/wfehr/django-time-wizard',
     download_url='https://github.com/wfehr/django-time-wizard/tarball/master',
     author='Wolfgang Fehr',
-    author_email='wolfgangfehr@fotopuzzle.de',
+    author_email='dev@wfehr.de',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
